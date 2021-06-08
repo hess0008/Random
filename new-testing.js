9173133630bae3f90e6cfe3749f7bd01e9b42635
Philip (Shou-Jeng) Huang <77962379+hess0008@users.noreply.github.com>
2021-06-08 19:41:35 +0800

    lol

--- new-testing.js
+++ new-testing.js
@@ -1,11 +1,11 @@
-var accordions = document.getElementsByClassName("accordion");
+let accordions = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < accordions.length; i++) {
     accordions[i].onclick = function(){
         this.classList.toggle("activated");
     
 
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.maxHeight){
         content.style.maxHeight = null;
     } else {
